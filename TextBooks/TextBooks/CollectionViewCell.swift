dfde4acfe25161be913c92dc2953aa1be9dfba4c
Tanmay Grandhisiri <grandhisiritanmay@gmail.com>
2019-10-17 22:41:40 +0530

    UIcollection1

---
+++ TextBooks/TextBooks/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  TextBooks
+//
+//  Created by Tanmay Grandhisiri on 17/10/19.
+//  Copyright © 2019 Tanmay Grandhisiri. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
