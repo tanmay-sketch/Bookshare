6299b8729b2aa2b8f9717cf1c60182e6684bb651
Tanmay Grandhisiri <grandhisiritanmay@gmail.com>
2019-12-27 22:13:51 +0530

    Books upload UI

---
+++ TextBooks/TextBooks/Controllers/UploadBooks.swift
@@ -0,0 +1,9 @@
+//
+//  UploadBooks.swift
+//  TextBooks
+//
+//  Created by Tanmay Grandhisiri on 27/12/19.
+//  Copyright © 2019 Tanmay Grandhisiri. All rights reserved.
+//
+
+import Foundation
---
+++ TextBooks/TextBooks/Controllers/UploadBooksViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UploadBooksViewController.swift
+//  TextBooks
+//
+//  Created by Tanmay Grandhisiri on 27/12/19.
+//  Copyright © 2019 Tanmay Grandhisiri. All rights reserved.
+//
+
+import UIKit
+
+class UploadBooksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
