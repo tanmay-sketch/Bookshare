2fa59345f2ffdb50233fb4b4ee5d29bd0a069083
Tanmay Grandhisiri <grandhisiritanmay@gmail.com>
2019-12-30 21:59:50 +0530

    DropDownlistforGrades (has errors)

--- TextBooks/TextBooks/Controllers/BooksUploadViewController.swift
+++ TextBooks/TextBooks/Controllers/BooksUploadViewController.swift
@@ -11,17 +11,31 @@ import FirebaseFirestore
 import FirebaseAuth
 import FirebaseStorage
 
+class CellClass: UITableViewCell{
+    
+}
+
 class BooksUploadViewController: UIViewController {
 
     
     @IBOutlet var imageView: UIImageView?
     @IBOutlet var txtGrades: UITextField?
+    @IBOutlet var txtTitle: UITextField?
+    @IBOutlet var txtSubject: UITextField?
+    @IBOutlet var txtAuthor: UITextField?
+    @IBOutlet var btnSelectGrade: UIButton!
     
     @IBOutlet var gradesPicker: UIPickerView?
     @IBOutlet var toolbar: UIToolbar?
     
+    let transparentView = UIView()
+    let tableView = UITableView()
+    var selectedButton = UIButton()
+    var dataSource = [Int]()
+    
     private let db = Firestore.firestore()
     private let storage = Storage.storage()
+    
 
     var imagePicker: UIImagePickerController!
     
@@ -34,7 +48,11 @@ class BooksUploadViewController: UIViewController {
         
         txtGrades?.inputView = gradesPicker
         txtGrades?.inputAccessoryView = toolbar
-  
+        
+        tableView.delegate = self as? UITableViewDelegate
+        tableView.dataSource = self as? UITableViewDataSource
+        tableView.register(CellClass.self, forCellReuseIdentifier: "Cell")
+
     }
     
     @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
@@ -43,6 +61,44 @@ class BooksUploadViewController: UIViewController {
     
     @IBAction func uploadImageTapped(_ sender:UIButton) {
         self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // DYNAMIC TEXT BUTTON
+    
+    func addTransparentView(frames: CGRect) {
+        let window = UIApplication.shared.keyWindow
+        transparentView.frame = window?.frame ?? self.view.frame
+        self.view.addSubview(transparentView)
+        
+        tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        self.view.addSubview(tableView)
+        tableView.layer.cornerRadius = 5
+        
+        transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        tableView.reloadData()
+        
+        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+        transparentView.addGestureRecognizer(tapgesture)
+        transparentView.alpha = 0
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0,initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.transparentView.alpha = 0.5
+            self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height + 5, width: frames.width, height: CGFloat(self.dataSource.count * 50))
+              }, completion: nil)
+    }
+    
+    @objc func removeTransparentView() {
+        let frames = selectedButton.frame
+      UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0,initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+      self.transparentView.alpha = 0
+      self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        }, completion: nil)
+    }
+
+    
+    @IBAction func onClickSelectGrade(_ sender: Any) {
+        dataSource = [6,7,8,9,10,11,12]
+        addTransparentView(frames: btnSelectGrade.frame)
+        selectedButton = btnSelectGrade
     }
     
     @IBAction func submitTapped(_ sender: UIButton) {
@@ -130,3 +186,25 @@ extension BooksUploadViewController : UIPickerViewDataSource, UIPickerViewDelega
         txtGrades?.text = "\(row+6)"
     }
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datasource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedButton.setTitle(dataSource[indexPath.row], for: .normal)
+        removeTransparentView()
+    }
+}
+    
